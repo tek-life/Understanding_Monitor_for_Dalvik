962f9e801e8c18d061712056808cdd5dc80184fa
Haifeng Li <hfli@marvell.com>
2014-01-05 19:13:44 +0800

    "why need synchronized" stage

---
+++ xiaoming.java
@@ -0,0 +1,49 @@
+class Person{
+	public String Name;
+	public double Length;
+};
+class Person_thread extends Thread{
+	private Person ming;
+	Person_thread(Person p, String name)
+	{
+		super(name);
+		this.ming=p;
+	}
+	public void run(){
+		for(int i=1;i<1000;i++){
+			if(getName().equals("Yao Ming"))
+			{
+				ming.Name="Yao Ming";
+				try{
+					Thread.sleep((int)Math.random()*1000);
+				}catch(InterruptedException e){
+					
+				}
+				ming.Length=2.26;
+				System.out.println(ming.Name+" "+ming.Length+" M");
+			}
+			else{
+				ming.Name="Guo Jingming";
+				try{
+					Thread.sleep((int)Math.random()*1000);
+				}catch(InterruptedException e){
+					
+				}
+				ming.Length=1.40;
+				System.out.println(ming.Name+" "+ming.Length+" M");
+			}
+		}
+		
+	}
+	
+}
+class XiaoMing{
+	static public void main(String args[]){
+		Person xiaoming=new Person();
+		Person_thread ming_A=new Person_thread(xiaoming,"Yao Ming");
+		Person_thread ming_B=new Person_thread(xiaoming,"Guo Jingming");
+		ming_A.start();
+		ming_B.start();
+	}
+	
+}
