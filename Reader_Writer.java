9f2be02c8cd662521ca2747652c82f648ed3d047
Haifeng Li <hfli@marvell.com>
2014-01-07 09:48:40 +0800

    Why need cooperation?

---
+++ Reader_Writer.java
@@ -0,0 +1,53 @@
+class Reader_Writer{
+	public static void main(String[] vargs){
+	Shared s= new Shared();
+	Writer_thread the_writer_thread=new Writer_thread(s);
+	Reader_thread the_reader_thread=new Reader_thread(s);
+	the_writer_thread.start();
+	the_reader_thread.start();
+	}
+}
+class Shared{
+	private char ch='\000';
+	void set_ch(char ch){this.ch=ch;}
+	char get_ch(){return ch;}
+}
+class Reader_thread extends Thread{
+	private Shared shared_object;
+	Reader_thread(Shared shared_object)
+	{
+		super("Reader");
+		this.shared_object=shared_object;
+	}
+	public void run()
+	{
+		char ch;
+		do{
+		try{
+			Thread.sleep((int)(Math.random() * 4000));
+		}catch(InterruptedException e){}
+		ch=shared_object.get_ch();
+		System.out.println(ch+getName());
+		}while(ch!='Z');
+	}
+}
+class Writer_thread extends Thread{
+	private Shared shared_object;
+	Writer_thread(Shared shared_object)
+	{
+		super("Writer");
+		this.shared_object=shared_object;
+	}
+	public void run()
+	{
+		for(char ch='A'; ch<='Z';ch++)
+		{
+			try{
+				Thread.sleep((int)(Math.random()*4000));
+			}catch(InterruptedException e){}
+			shared_object.set_ch(ch);
+			System.out.println(ch+getName());
+		}
+	}
+
+}
